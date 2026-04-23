aca55f35b34bdc2f8079ac4cb7b11ef07108fd1d
paulofuchshuber <paulo12372@gmail.com>
2026-04-22 22:51:07 -0300

    change port 4 render

--- app.py
+++ app.py
@@ -96,4 +96,7 @@ def aggregate():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 10000))
+    )
